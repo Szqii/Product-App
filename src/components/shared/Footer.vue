165eff8b3ca50e7eff3987fafdae1bb0f4d747ff
Szqii <szqii.altn@gmail.com>
2021-01-14 03:43:24 +0300

    Add Navbar and Footer

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav
+      class="navbar fixed-bottom navbar-light d-flex justify-content-between"
+      style="background-color: #e3f2fd;"
+    >
+      <span class="navbar-text mr-auto">
+        <strong>VueJS Ürün İşlemleri Uygulaması</strong> | Sezgi Altan
+      </span>
+      <div class="values d-flex align-items-center justify-content-center">
+      <span class="navbar-text navbar-nav my-2 my-lg-0 p-2">
+        <strong>Alış Tutarı</strong>
+        <span class="badge bg-danger">{{getTradeResult.purchase | currency}}</span>
+      </span>
+      <span class="navbar-text navbar-nav my-2 my-lg-0 p-2">
+        <strong>Satış Tutarı</strong>
+        <span class="badge bg-success">{{getTradeResult.sale | currency}}</span>
+      </span>
+      <span class="navbar-text navbar-nav my-2 my-lg-0 p-2">
+        <strong>Bakiye</strong>
+        <span class="badge bg-primary px-4">{{getTradeResult.balance | currency}}</span>
+      </span>
+      </div>
+    </nav>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+computed : {
+  ...mapGetters(["getTradeResult"])
+}
+
+};
+</script>
+
+<style lang="scss" scoped>
+nav{
+    padding: 1rem;
+}
+</style>
---
+++ src/components/shared/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-light"
+  >
+    <router-link class="navbar-brand active" to="/" tag="a">
+      Ürün Listesi
+    </router-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link active-class="active" tag="li" to="/add">
+          <a class="nav-link">Ürün Girişi</a>
+        </router-link>
+        <router-link active-class="active" tag="li" to="/remove">
+          <a class="nav-link">Ürün Çıkışı</a>
+        </router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  padding: 1rem;
+  background-color: rgba(#ff8b8b, .8);
+}
+</style>
